ffe3f1041979074478d4aa2b0e7639bf7b2990f8
headStyleColorRed <headStyleColorRed@gmail.com>
2022-01-27 18:51:11 +0100

    working tests

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 Let's start with the postres container that will save all data on the postgres_data volume
 
 ```sh
-docker compose up -d
+docker compose up --build -d
 ```
 
 If you want to check if the database is up and running do a `docker ps` and then display the container's logs by a `docker logs 3290f9616b1b`
@@ -31,40 +31,4 @@ echo DATABASE_URL=postgres://root:1234@localhost/chronos > .env
 Once this is done we can create the migrations running
 ```sh
 diesel migration run
-```
-
-## Check DB
-If you want to connect to the db you can run this command to join the docker
-command line and then enter the psql cli as root with the password provided on the docker-compose
-
-```sh
-docker exec -it postgres_db bash
-psql -h postgres_db -d chronos -U root ;
-```
-Once you are inside, you can run the `\l` command and you'll be shown a list of databases
-```
-chronos=# \l
-                             List of databases
-   Name    | Owner | Encoding |  Collate   |   Ctype    | Access privileges 
------------+-------+----------+------------+------------+-------------------
- chronos   | root  | UTF8     | en_US.utf8 | en_US.utf8 | 
- postgres  | root  | UTF8     | en_US.utf8 | en_US.utf8 | 
- template0 | root  | UTF8     | en_US.utf8 | en_US.utf8 | =c/root          +
-           |       |          |            |            | root=CTc/root
- template1 | root  | UTF8     | en_US.utf8 | en_US.utf8 | =c/root          +
-           |       |          |            |            | root=CTc/root
-(4 rows)
-```
-The table we created is the chronos, so we'll connect to that db by running `\c chronos`, which will allow us to list the relations inside that db with another `\d`.
-```
-chronos=# \d
-                   List of relations
- Schema |            Name            |   Type   | Owner 
---------+----------------------------+----------+-------
- public | __diesel_schema_migrations | table    | root
- public | punches                    | table    | root
- public | punches_id_seq             | sequence | root
- public | users                      | table    | root
- public | users_id_seq               | sequence | root
-(5 rows)
 ```
--- src/tests/user/tests.rs
+++ src/tests/user/tests.rs
@@ -2,11 +2,15 @@
 
 
 pub mod user_tests {
+    use futures::{Future, future::{BoxFuture, ok}};
     use graphql_client::{GraphQLQuery, Response};
+    struct UserData {
+        id: i64,
+        name: String,
+        status: i64,
+    }
 
-    #[tokio::main]
-    #[test]
-    async fn get_all_users() {
+    async fn get_all_users() -> Result<i32, ()> {
 
         use super::super::models::all_users_query::user::{AllUsersQuery, all_users_query, all_users_query::Variables};
 
@@ -14,25 +18,45 @@ pub mod user_tests {
         let request_body = AllUsersQuery::build_query(variables);
         let client = reqwest::Client::new();
         let res = client.post("http://localhost:8080/graphql").json(&request_body).send().await.unwrap();
-        let response_body: Response<all_users_query::ResponseData> = res.json().await.unwrap();
-
-        println!("{:#?}", response_body);
+        let response: Response<all_users_query::ResponseData> = res.json().await.unwrap();
+        let response_data = response.data.unwrap();
+        
+        Ok(response_data.all_users.len() as i32)
     }
 
 
-    #[tokio::main]
-    #[test]
-    async fn create_user() {
+    async fn create_user(name: &str) -> Result<UserData, ()> {
         use super::super::models::create_user_mutation::user::{CreateUserMutation, create_user_mutation, create_user_mutation::Variables};
     
-        let input = create_user_mutation::CreateUserInput { name: "Rodrigo".to_string() };
+        let input = create_user_mutation::CreateUserInput { name: name.to_string() };
         let variables: Variables = Variables { input };
     
         let request_body = CreateUserMutation::build_query(variables);
         let client = reqwest::Client::new();
         let res = client.post("http://localhost:8080/graphql").json(&request_body).send().await.unwrap();
-        let response_body: Response<create_user_mutation::ResponseData> = res.json().await.unwrap();
-    
-        println!("{:#?}", response_body);
+        let response: Response<create_user_mutation::ResponseData> = res.json().await.unwrap();
+        let response_data = response.data.unwrap();
+        let data_user = response_data.create_user;
+
+        let user =  UserData {
+            id: data_user.id,
+            name: data_user.name,
+            status: data_user.status,
+        };
+
+        Ok(user)
+    }
+
+    #[tokio::main]
+    #[test]
+    async fn user_flow() {
+        // Get current users
+        let users_count: i32 = get_all_users().await.unwrap();
+
+        // Create new user
+        let new_user = create_user("Michael Scott Carbilias Serpentico").await.unwrap();
+
+        println!("Users => {}", users_count);
+        println!("Name => {}", new_user.name);
     }
 }
