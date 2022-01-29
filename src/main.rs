ec3105be79804cce417be840e685825db83f8e82
headStyleColorRed <headStyleColorRed@gmail.com>
2022-01-29 11:20:57 +0100

    adding auth wrapper

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,19 @@
 #[macro_use]
 extern crate diesel;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, dev::Service};
 mod context;
 mod db;
 mod endpoints;
 mod graphql;
 mod models;
 mod operations;
-mod schema;
-mod utils;
 mod rest;
+mod schema;
 mod tests;
+mod utils;
 
+use futures::FutureExt;
 use rest::generic::health;
 
 #[actix_rt::main]
@@ -30,6 +31,13 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .data(pool.clone())
+            .wrap_fn(|req, srv| {
+                println!("Hi from start. You requested: {}", req.path());
+                srv.call(req).map(|res| {
+                    println!("Hi from response");
+                    res
+                })
+            })
             .route("/", web::get().to(health))
             .configure(endpoints::graphql_endpoints)
     })
