4e3eaea995f1953b24eec3a386dbdb015617dfa6
headStyleColorRed <headStyleColorRed@gmail.com>
2022-01-30 13:35:40 +0100

    adding jwt decoding

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,8 +1,10 @@
 use super::punch::Punch;
 use crate::{context::GraphQLContext, operations::punches::Punches};
 use crate::schema::users;
+use actix_web::{dev, Error, FromRequest, HttpRequest};
 use diesel::Queryable;
-use juniper::{FieldResult, GraphQLInputObject};
+use juniper::{FieldResult, GraphQLInputObject, FieldError};
+use futures::future::Ready;
 // The core data type undergirding the GraphQL interface
 #[derive(Queryable, Debug)]
 pub struct User {
@@ -29,6 +31,15 @@ impl User {
     pub fn history(context: &GraphQLContext) -> FieldResult<Vec<Punch>> {
         Punches::all_punches_for_user(context, self.id)
     }
+}
+// FieldResult::Err(FieldError::from("This user is already clocked in"))
+impl FromRequest for User {
+      type Error = Error;
+      type Future = Ready<Result<Self, Self::Error>>;
+      type Config = ();
+
+      fn from_request(req: &HttpRequest, payload: &mut dev::Payload) -> Self::Future { }
+
 }
 
 // The GraphQL input object for creating TODOs
